05a9665054bfd5ffcee85b7f7fb9c7f6952112d4
VasysMS <vasiavasia10000@gmail.com>
2024-04-07 01:32:21 +0300

    Added get_all_bells and add_bell functions

--- web/app.py
+++ web/app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from pydub import AudioSegment
+from datetime import datetime
+import uuid
+import os
+import db
 
 app = Flask(__name__)
+username = "John Doe"
 
 # Главная страница
 @app.route('/')
 def index():
-    username = "John Doe"
     # Создание переменной newBell
     newBell = {
         "time": "",
@@ -14,7 +18,9 @@ def index():
         "info": "",
         "uploaderName": username
     }
-    return render_template('index.html', newBell=newBell)
+    current_time = datetime.now()
+    
+    return render_template('index.html', newBell=newBell, bells=db.get_all_bells(), current_time=current_time, datetime=datetime)
 
 # Страница логинизации
 @app.route('/login')
@@ -24,10 +30,12 @@ def login():
 @app.route('/audio', methods=['POST'])
 def upload_audio():
     audio_file = request.files['audio']
-    # Сохраняем загруженный файл на сервере
-    audio_file.save('static/audio/' + audio_file.filename)
+
+    os.makedirs(os.path.join('static', 'audio'), exist_ok=True)
+    unique_filename = f"{uuid.uuid4()}-{audio_file.filename}"
+
+    audio_file.save(os.path.join('static', 'audio', unique_filename))
     
-    # Определяем длительность аудио
     audio = AudioSegment.from_file('static/audio/' + audio_file.filename)
     duration_seconds = len(audio) / 1000
     
@@ -35,6 +43,25 @@ def upload_audio():
         'filename': audio_file.filename,
         'duration': duration_seconds
     }
+
+@app.route('/create_bell', methods=['POST'])
+def create_bell():
+    info = request.form['info']
+    time_str = request.form['time']
+    audio_file = request.files['audio']
+
+    unique_filename = f"{uuid.uuid4()}-{audio_file.filename}"
+
+    audio_file.save(os.path.join('static', 'audio', unique_filename))
+
+    audio = AudioSegment.from_file(os.path.join('static/audio', unique_filename))
+    duration = len(audio) / 1000
+
+    time = datetime.strptime(time_str, '%Y-%m-%dT%H:%M:%S')
+
+    db.add_bell(info, time, unique_filename, duration, username)
+
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- web/db.py
+++ web/db.py
@@ -1,32 +1,51 @@
 import sqlite3
 
-# Создаем соединение с базой данных
 conn = sqlite3.connect('database.db')
 c = conn.cursor()
 
-# Создаем таблицу для хранения значения дивергенции
 c.execute('''CREATE TABLE IF NOT EXISTS bells (
-                id INTEGER PRIMARY KEY,
-                value REAL
+                Id INTEGER PRIMARY KEY,
+                Info TEXT,
+                Time DATETIME,
+                AudioFilePath TEXT,
+                Duration TIMESPAN,
+                UploaderName TEXT
             )''')
 
-# Сохраняем изменения и закрываем соединение
 conn.commit()
 conn.close()
 
-# Функция для получения текущего значения дивергенции
-def get_divergence():
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    c.execute('SELECT value FROM divergence ORDER BY id DESC LIMIT 1')
-    divergence = c.fetchone()[0]
-    conn.close()
-    return round(divergence, 6)  # Округляем значение до 6 знаков после запятой
+def seconds_to_hms(seconds):
+    minutes, seconds = divmod(int(seconds), 60)
+    hours, minutes = divmod(minutes, 60)
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
 
-# Функция для обновления значения дивергенции
-def update_divergence(value):
+def add_bell(info, time, audio_file_path, duration, uploader_name):
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
-    c.execute('INSERT INTO divergence (value) VALUES (?)', (value,))
+
+    c.execute('''INSERT INTO bells (Info, Time, AudioFilePath, Duration, UploaderName)
+                 VALUES (?, ?, ?, ?, ?)''',
+              (info, time, audio_file_path, seconds_to_hms(duration), uploader_name))
+
     conn.commit()
     conn.close()
+
+def get_all_bells():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    c.execute('SELECT * FROM bells ORDER BY Time ASC')
+    bells = [
+        {
+            'id': row[0],
+            'info': row[1],
+            'time': row[2],
+            'audioFilePath': row[3],
+            'duration': row[4],
+            'uploaderName': row[5]
+        } for row in c.fetchall()
+    ]
+
+    conn.close()
+    return bells
